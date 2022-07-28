213623f5e5cea7bf1350698fc1dc42ff77f51969
madjanorjedidiah <jmadjanor6@gmail.com>
2022-07-28 22:12:09 +0000

    settingss

--- martor_demo/martor_demo/settings.py
+++ martor_demo/martor_demo/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
-# import tempfile
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -146,3 +145,4 @@ MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
